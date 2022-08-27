06094eae51cc178edc139841c5e022e3e0e30a67
reten <glay5@DESKTOP-CA1LDPU>
2022-08-27 23:47:43 +0900

    プロジェクト ファイルを追加します。

---
+++ DataDisplay/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace DataDisplay
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tbxRxData = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.btnScan = new System.Windows.Forms.Button();
+            this.cmbCOMPort = new System.Windows.Forms.ComboBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.SuspendLayout();
+            // 
+            // tbxRxData
+            // 
+            this.tbxRxData.Location = new System.Drawing.Point(72, 254);
+            this.tbxRxData.Name = "tbxRxData";
+            this.tbxRxData.Size = new System.Drawing.Size(100, 22);
+            this.tbxRxData.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(311, 254);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(550, 254);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 2;
+            // 
+            // btnScan
+            // 
+            this.btnScan.Location = new System.Drawing.Point(51, 26);
+            this.btnScan.Name = "btnScan";
+            this.btnScan.Size = new System.Drawing.Size(75, 23);
+            this.btnScan.TabIndex = 3;
+            this.btnScan.Text = "re-scan";
+            this.btnScan.UseVisualStyleBackColor = true;
+            this.btnScan.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // cmbCOMPort
+            // 
+            this.cmbCOMPort.FormattingEnabled = true;
+            this.cmbCOMPort.Location = new System.Drawing.Point(161, 26);
+            this.cmbCOMPort.Name = "cmbCOMPort";
+            this.cmbCOMPort.Size = new System.Drawing.Size(121, 23);
+            this.cmbCOMPort.TabIndex = 4;
+            this.cmbCOMPort.Text = "-Select-";
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(311, 26);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 5;
+            this.btnOpen.Text = "open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(401, 26);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 6;
+            this.btnClose.Text = "close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.BaudRate = 115200;
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.cmbCOMPort);
+            this.Controls.Add(this.btnScan);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.tbxRxData);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbxRxData;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button btnScan;
+        private System.Windows.Forms.ComboBox cmbCOMPort;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnClose;
+        private System.IO.Ports.SerialPort serialPort1;
+    }
+}
+
---
+++ DataDisplay/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace DataDisplay
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            scanCOMPorts();
+        }
+
+        private void scanCOMPorts()
+        {
+            cmbCOMPort.Items.Clear();
+            string[] ports = SerialPort.GetPortNames();
+            foreach (string p in ports)
+            {
+                cmbCOMPort.Items.Add(p);
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            scanCOMPorts();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.PortName = cmbCOMPort.Text; // COM名設定
+                serialPort1.Open();                     // ポート接続
+                btnOpen.Enabled = false;                // 接続　Off
+                btnClose.Enabled = true;                // 切断　On
+                btnScan.Enabled = false;                // 更新　Off
+                cmbCOMPort.Enabled = false;             // COMリスト　Off
+                //btnSend.Enabled = true;                 // 送信　On
+                //tbxRxData.Clear();                      // 画面クリア
+                //tbxRxData.AppendText("Connected\r\n");  // 接続と表示
+            }
+            catch
+            {
+                btnClose_Click(this, null);     // 切断ボタンを押す
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            btnOpen.Enabled = true;             // 接続　On
+            btnClose.Enabled = false;           // 切断　Off
+            btnScan.Enabled = true;             // 更新　On
+            cmbCOMPort.Enabled = true;          // COMリスト　On
+            //btnSend.Enabled = false;            // 送信　Off
+            try
+            {
+                serialPort1.DiscardInBuffer();  // 入力バッファを破棄
+                serialPort1.DiscardOutBuffer(); // 出力バッファを破棄
+                serialPort1.Close();             // COMポートを閉じる
+            }
+            catch { };
+        }
+
+        delegate void SetTextCallback(string text);
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                SetText(serialPort1.ReadExisting());
+            }
+            catch
+            {
+                btnClose_Click(this, null);
+            }
+        }
+
+        private void SetText(string text)
+        {
+            if (tbxRxData.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText);
+                Invoke(d, new object[] { text });
+            }
+            else
+            {
+                tbxRxData.AppendText(text);
+            }
+        }
+    }
+}
